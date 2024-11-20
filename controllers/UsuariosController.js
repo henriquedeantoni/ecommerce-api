6a956ad8b1b53d75c0874930ae0fadce1c05bde0
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-11-20 18:26:58 -0300

    atualizado userController

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -33,6 +33,8 @@ class UsuarioController{
     store(req, res, next){
         const { nome, email, password} = req.body;
 
+        if(!nome || !email || !password) return res.status(422).json({errors: "Preencha todos os campos de cadastro."})
+
         const usuario = new Usuario({ nome, email });
         usuario.setSenha(password);
 
@@ -79,4 +81,58 @@ class UsuarioController{
             return res.json({ usuario: usuario.enviarAuthJSON() });
         }).catch(next);
     }
+
+    // RECOVERY /
+
+    // GET /recuperar-senha
+
+    ShowRecovery(req, res, next){
+        return res.render('recovery', {error: null, success: null});
+    }
+
+    // POST /recuperar-senha
+    CreateRecovery(req, res, next){
+        const {email} = req.body;
+        if(!email) return res.render('recovery', {error: "preencha com seu email", success: null});
+        
+        Usuario.findOne({email}).then((usuario)=> {
+            if(!usuario) return res.render("recovery", { error: "Não existe usuário com este email", success: null});
+            const recoveryData = usuario.criarTokenRecupercaoSenha();
+            return usuario.save().then(() =>{
+                return res.render("recovery", {error: null, success: true});
+            }).catch(next);
+        }).catch(next);
+    }
+
+    //GET / Senha Recuperada
+    showCompleteRecovery(req, res, next){
+        if(!req.query.token) return res.render("recovery", {error: "Token não identificado", success: null});
+        Usuario.findOne({"recovery.token": req.query.token}).then(usuario => {
+            if(!usuario) return res.render("recovery", { error: "Não existe usuário com este token", success: null});
+            if(new Date(usuario.recovery.date) < new Date() ) return res.render("recovery", {error: "Token expirado. Tente novamente", success: null})
+                return res.render("recovery/store", { error: null, success: null, token: req.query.token});
+        }).catch(next);
+    }
+
+    // POST / Senha-recuperad
+
+    completeRecovery(req, res, next){
+        const {token, password} = req.body;
+        if(!token || !password ) return res.render("recovery/store", {error: "Preencha novamente com sua nova senha", success: null, token: token});
+        Usuario.findOne({ "recovery.token": token}).then(usuario => {
+            if(!usuario) return res.render("recovery", {error: "Usuario não identificado", success: null});
+
+            usuario.finalizarTokenRecuperarSenha();
+            usuario.setSenha(password);
+
+            return usuario.save().then(()=>{
+                return res.render("recovery/store",{
+                    error: null, 
+                    success: "Senha alterada com sucesso. Tnte novamente fazer login.", 
+                    token: null
+                });
+            }).catch(next);
+        });
+    }
+    
 }
