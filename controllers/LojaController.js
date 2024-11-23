3ec339954a9af428d6d0d0cabdff212916722ef1
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-11-23 18:15:42 -0300

    feat: adicionado classe LojaController

---
+++ controllers/LojaController.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Loja = mongoose.model("Loja");
+
+class LojaController {
+
+    //GET /
+    index(req, res, next){
+
+    }
+
+    //GET /:id
+    show(req, res, next){
+
+    }
+
+    //POST /
+    store(req, res, next){
+
+    }
+
+    //PUT /:id
+    update(req, res, next){
+
+    }
+    
+    //DELETE /
+    remove(req, res, next){
+        
+    }
+
+
+}
+
+module.exports = LojaController
