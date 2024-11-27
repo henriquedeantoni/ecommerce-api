47ebfbbc9d4192abf3582428f7f4cebb91baa12b
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-11-26 23:38:52 -0300

    feat: modificado usuariocontroller para validacao

--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -5,11 +5,11 @@ const UsuarioController = require("../../../controllers/UsuarioController");
 const Validation =  require("express-validation");
 const {usuarioValidation} = require("../../../controllers/validacoes/usuarioValidation");
 
-
 const usuarioController = new UsuarioController();
+
 router.post("/login", Validation(usuarioValidation.login), usuarioController.login);
-router.post("/registrar", usuarioController.store);
-router.put("/", auth.required ,usuarioController.update);
+router.post("/registrar", Validation(usuarioValidation.store), usuarioController.store);
+router.put("/", auth.required , Validation(usuarioValidation.update), usuarioController.update);
 router.delete("/", auth.required, usuariosController.remove);
 
 router.get("/recuperar-senha", usuariosController.showRecovery);
@@ -18,7 +18,7 @@ router.get("/senha-recuperada", usuarioController.showCompleteRecovery);
 router.post("/senha-recuperada", usuarioController.completeRecovery);
 
 router.get("/", auth.required, usuarioController.index);
-router.get("/:id", auth.required, usuarioController.show);
+router.get("/:id", auth.required, Validation(usuarioValidation.show), usuarioController.show);
 
 
 module.exports = router;
