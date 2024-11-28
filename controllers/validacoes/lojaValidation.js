597ab5669486608a4d1b5ef373a57806ce0ccace
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-11-27 22:22:56 -0300

    concluida validacao loja

--- controllers/validacoes/lojaValidation.js
+++ controllers/validacoes/lojaValidation.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const BaseJoi = require("joi");
 const Extension = require("joi-date-extensions");
+const Joi = BaseJoi.extend(Extension);
 
 const Usuario = mongoose.model("Usuario");
-
 const Loja = mongoose.model("Loja");
 
 const LojaValidation = {
@@ -20,7 +20,45 @@ const LojaValidation = {
         if(!usuario.loja.toString() !== loja) return res.sendStatus(401);
         next();
     }).catch(next);    
-    }  
+    },
+    show: {
+        params: {
+            id: Joi.string().alphanum().length(24).required()
+        }
+    },
+    store: {
+        body: {
+            nome: Joi.string().required(),
+            cnpj: Joi.string().length(18).required(), 
+            email: Joi.string().email().required(), 
+            telefones: Joi.array().items(Joi.string()).required(), 
+            endereco: Joi.object({
+                local:  Joi.string().required(),
+                numero: Joi.string().required(),
+                complemento: Joi.string().optional(),
+                bairro: Joi.string().required(),
+                cidade: Joi.string().required(),
+                CEP: Joi.string().required()
+            }).required()
+        }
+    },
+    update: {
+        body:
+        {
+            nome: Joi.string().optional(),
+            cnpj: Joi.string().length(18).required(), 
+            email: Joi.string().email().required(), 
+            telefones: Joi.array().items(Joi.string()).required(), 
+            endereco: Joi.object({
+                local:  Joi.string().required(),
+                numero: Joi.string().required(),
+                complemento: Joi.string().optional(),
+                bairro: Joi.string().required(),
+                cidade: Joi.string().required(),
+                CEP: Joi.string().required()
+            }).optional()
+        }
+    }
 };
 
 module.exports = { LojaValidation };
--- routes/api/v1/lojas.js
+++ routes/api/v1/lojas.js
@@ -1,15 +1,17 @@
 const router = require("express").router();
-const {LojaValidation} = require("../../../Controller/validacoes/lojaValidator");
 const auth = require("../../auth");
-const LojaController = require("../../../controllers/LojaController");
+
+const Validation = require("express-validation");
+const {LojaValidation} = require("../../../Controller/validacoes/lojaValidator");
 
 const lojaController = new LojaController();
+const LojaController = require("../../../controllers/LojaController");
 
 router.get("/", lojaController.index);
-router.get("/:id", lojaController.show);
+router.get("/:id", Validation(LojaValidation.show), lojaController.show);
 
-router.post("/", auth.required, lojaController.store);
-router.put("/:id", auth.required, LojaValidation.admin,  lojaController.update);
-router.delete("/:id", auth.required, LojaValidation.admin, lojaController.remove);
+router.post("/",  auth.required, Validation(LojaValidation.store), lojaController.store);
+router.put("/:id", auth.required, LojaValidation.admin, Validation(LojaValidation.update), lojaController.update);
+router.delete("/:id", auth.required, LojaValidation.admin,  lojaController.remove);
 
 module.exports = router;
